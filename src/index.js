1ccc991ccb95486e8673dd0eaefeaf14a1e51587
Wannes Wagemans <58140918+wannes-wagemans@users.noreply.github.com>
2022-01-25 16:03:10 +0100

    commit -m fixed bug

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore } from 'redux';
 import rootReducer from './reducers';
-import thunk from "redux-thunk"
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
